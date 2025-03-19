e0d51616bfeb8ac8753ffb7a62cc9b6fe6bd9d1d
phamgianglam1996 <phamgianglam1996@gmail.com>
2025-03-20 00:12:04 +0700

    update converter

--- src/main/java/com/example/todolist/task/converter/TaskRequestDTOToTaskConverter.java
+++ src/main/java/com/example/todolist/task/converter/TaskRequestDTOToTaskConverter.java
@@ -1,16 +1,37 @@
 package com.example.todolist.task.converter;
 
+import com.example.todolist.profile.Profile;
+import com.example.todolist.profile.ProfileException;
+import com.example.todolist.profile.ProfileRepository;
 import com.example.todolist.task.Task;
 import com.example.todolist.task.dto.TaskRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
+import static com.example.todolist.helper.Helper.convertISoStringToZoneDateTime;
+
 @Component
 public class TaskRequestDTOToTaskConverter implements Converter <TaskRequestDTO, Task> {
+
+    private ProfileRepository profileRepository;
+
+    public TaskRequestDTOToTaskConverter(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
     @Override
     public Task convert(TaskRequestDTO taskRequestDTO){
         Task task = new Task();
-        if
+        Profile profile = this.profileRepository
+                .findById(taskRequestDTO.ownerId())
+                .orElseThrow(() -> new ProfileException.UserNotFoundException(taskRequestDTO.ownerId()));
+        task.setTitle(taskRequestDTO.title());
+        task.setDueDate(convertISoStringToZoneDateTime(taskRequestDTO.dueDate()));
+        task.setStatus(taskRequestDTO.status());
+        task.setDescription(taskRequestDTO.description());
+        task.setOwner(profile);
+
         return  task;
     }
 }
