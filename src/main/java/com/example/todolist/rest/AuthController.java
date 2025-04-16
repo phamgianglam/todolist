1e7bb2f078d6d1550ca0bc7945fb45c2c5b12518
phamgianglam1996 <phamgianglam1996@gmail.com>
2025-04-16 10:30:07 +0700

    reformat ut

--- src/main/java/com/example/todolist/rest/AuthController.java
+++ src/main/java/com/example/todolist/rest/AuthController.java
@@ -12,7 +12,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
--- src/main/java/com/example/todolist/rest/TaskController.java
+++ src/main/java/com/example/todolist/rest/TaskController.java
@@ -8,8 +8,8 @@ import com.example.todolist.dto.task.TaskResponseDTO;
 import com.example.todolist.model.Task;
 import com.example.todolist.service.TaskService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import java.util.List;
 import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,7 +34,8 @@ public class TaskController {
   }
 
   @PostMapping("/")
-  public ResponseEntity<TaskResponseDTO> createTask(@Valid @RequestBody TaskRequestDTO taskRequestDTO) {
+  public ResponseEntity<TaskResponseDTO> createTask(
+      @Valid @RequestBody TaskRequestDTO taskRequestDTO) {
     Task task = this.taskRequestDTOToTaskConverter.convert(taskRequestDTO);
 
     if (task.getOwner() != null) {
