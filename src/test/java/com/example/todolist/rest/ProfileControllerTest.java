25365e1cae3ad407a108bbf7ba820dd6557a6f5b
phamgianglam1996 <phamgianglam1996@gmail.com>
2025-04-17 12:36:30 +0700

    update test

--- src/test/java/com/example/todolist/rest/ProfileControllerTest.java
+++ src/test/java/com/example/todolist/rest/ProfileControllerTest.java
@@ -24,12 +24,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class ProfileControllerTest {
 
--- src/test/java/com/example/todolist/rest/TaskControllerTest.java
+++ src/test/java/com/example/todolist/rest/TaskControllerTest.java
@@ -22,12 +22,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class TaskControllerTest {
   @Autowired MockMvc mockMvc;
