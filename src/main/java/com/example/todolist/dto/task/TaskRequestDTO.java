b9a54276f703d03c06749e0628b0b3c628bca405
phamgianglam1996 <phamgianglam1996@gmail.com>
2025-04-17 11:17:14 +0700

    verify

--- src/main/java/com/example/todolist/dto/task/TaskRequestDTO.java
+++ src/main/java/com/example/todolist/dto/task/TaskRequestDTO.java
@@ -19,5 +19,4 @@ public record TaskRequestDTO(
         @NotNull
         @Schema(example = "2026-02-20T20:19:04Z")
         String dueDate,
-    @Schema(example = "1") Long ownerId) {
-}
+    @Schema(example = "1") Long ownerId) {}
